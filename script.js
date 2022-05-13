22d04b318e1c9509e8945a8a043b2b663d5ae5da
niskuldeep <kuldeepnishad.singh@gmail.com>
2022-05-13 13:22:57 +0530

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector(".container");
+let ct = 1;
+
+const getStudent = () => {
+  for (let i = 0; i < 25; i++) {
+    const html = `<div id="student">Student ${ct}</div>`;
+    ct++;
+    container.insertAdjacentHTML("beforeend", html);
+  }
+};
+
+container.addEventListener("scroll", () => {
+  const { scrollHeight, scrollTop, clientHeight } = container;
+  if (scrollHeight <= Math.ceil(scrollTop + clientHeight)) {
+    getStudent();
+  }
+});
+
+getStudent();
